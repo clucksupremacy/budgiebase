c2315f2279f881844bff1fefa51d5969fa83b094
clucksupremacy <cherrycluckcluck@gmail.com>
2024-03-30 18:19:41 -0700

    phoenix page

---
+++ budgiebase/src/app/phoenix.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from 'next/link';
+import styles from "./page.module.css";
+
+export default function Phoenix() {
+  return (
+    <main className={styles.main}>
+      <div>
+        <h1>Phoenix</h1>
+        <img src='/pictures/phoenix.png' />
+      </div>
+    </main>
+  );
+}
