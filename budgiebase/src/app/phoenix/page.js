5f8525c770feeffdc65543b82f5fdeaf3ce2a319
clucksupremacy <cherrycluckcluck@gmail.com>
2024-06-19 16:41:34 -0700

    save for later

--- budgiebase/src/app/phoenix/page.js
+++ budgiebase/src/app/phoenix/page.js
@@ -1,13 +1,33 @@
 import Image from "next/image";
 import Link from 'next/link';
 import styles from "../page.module.css";
+import { useEffect, useState } from 'react';
+import db from '../db';
 
 export default function Phoenix() {
+    const [age, setFavoriteToy] = useState(null);
+
+    useEffect(() => {
+        async function fetchFavoriteToy() {
+            try {
+                const { rows } = await db.query('SELECT favoritetoy FROM budgies WHERE id = 3', ['Mugly']);
+                if (rows.length > 0) {
+                setFavoriteToy(rows[0].favoritetoy);
+                }
+            } catch (error) {
+                console.error('Error fetching age:', error);
+            }
+        }
+
+        fetchFavoriteToy();
+    }, []);
+
   return (
     <main className={styles.main}>
       <div>
         <h1>Phoenix</h1>
         <img src='/pictures/phoenix.png' />
+        {favoritetoy !== null ? <p>{favoritetoy}</p> : <p>Loading...</p>}
       </div>
     </main>
   );
